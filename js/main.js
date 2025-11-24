1d2717b9ea1faf3224b08e342750f370fedab8ae
JTC <joetcld@gmail.com>
2025-11-24 12:10:28 -0600

    Se agrega index.html y main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let numA= prompt("Ingresa el primer número");
+let numB= prompt("Ingresa el segundo número");
+let numC= prompt("Ingresa el tercer número");
+
+const arr = [numA, numB, numC];
+function comparar(a,b) { 
+    return a-b
+}
+console.log("Así queda el ordenamiento : ", arr.sort(comparar));
